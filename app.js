618b20cbc4662c0e986b74b95a6ce48fa84502e3
jeremiah <100346840+Ezekwu@users.noreply.github.com>
2022-08-06 20:38:40 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const labels = [...document.querySelectorAll('.label')];
+const contents = [...document.querySelectorAll('.content')];
+
+labels.forEach((label)=>{
+    label.addEventListener('click', (e) => {
+        label.nextElementSibling.classList.toggle('show')
+    })
+})
+
+
